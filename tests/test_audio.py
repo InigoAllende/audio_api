1712f5a788bd0071e9b502a30450ceccb4ea5018
Inigo <iallende2@gmail.com>
2023-12-08 17:15:40 +0100

    format

--- tests/test_audio.py
+++ tests/test_audio.py
@@ -1,18 +1,22 @@
 import glob
-from http import HTTPStatus
 import io
 import os
-from config import settings
+from http import HTTPStatus
+
 import pytest
+
+from config import settings
 
 FILE_NAME = "test_file.mp3"
 FILE_CONTENT = b"\x00\x00\x00\x00\x00\x00\x00\x00\x01\x01\x01\x01\x01\x01"
 
+
 @pytest.fixture(scope="module", autouse=True)
 def clean_up():
     yield
-    for f in glob.glob(os.path.join(settings.STORAGE_PATH, '*')):
+    for f in glob.glob(os.path.join(settings.STORAGE_PATH, "*")):
         os.remove(os.path.join(settings.STORAGE_PATH, f))
+
 
 @pytest.mark.dependency()
 def test_audio_upload(client):
@@ -25,6 +29,7 @@ def test_audio_upload(client):
 
     assert response.status_code == HTTPStatus.CREATED
     assert os.path.exists(os.path.join(settings.STORAGE_PATH, FILE_NAME))
+
 
 @pytest.mark.dependency(depends=["test_audio_upload"])
 def test_audio_download(client):
@@ -43,21 +48,29 @@ def test_audio_download_bad_file_name(client, filename):
     )
     assert response.status_code == HTTPStatus.NOT_FOUND
 
+
 @pytest.mark.dependency(depends=["test_audio_upload"])
 def test_audio_volume_modification(client, mocker):
     filename = "test_file.mp3"
 
     mocked_function = mocker.patch("api.routes.audio.AudioSegment.from_file")
     response = client.put(
-            url=f"/audio/{filename}/adjust_volume", headers={"x-api-key": "TEST_KEY"}, json={"volume_increase": 1}
-        )
-    
+        url=f"/audio/{filename}/adjust_volume",
+        headers={"x-api-key": "TEST_KEY"},
+        json={"volume_increase": 1},
+    )
+
     assert response.status_code == HTTPStatus.NO_CONTENT
-    assert mocked_function.called_once_with(os.path.join(settings.STORAGE_PATH, filename))
+    assert mocked_function.called_once_with(
+        os.path.join(settings.STORAGE_PATH, filename)
+    )
+
 
 @pytest.mark.parametrize("filename", [None, "", "Not_existing", 1231])
 def test_audio_volume_modification_bad_file(client, filename):
     response = client.put(
-            url=f"/audio/{filename}/adjust_volume", headers={"x-api-key": "TEST_KEY"}, json={"volume_increase": 1}
-        )
+        url=f"/audio/{filename}/adjust_volume",
+        headers={"x-api-key": "TEST_KEY"},
+        json={"volume_increase": 1},
+    )
     assert response.status_code == HTTPStatus.NOT_FOUND
